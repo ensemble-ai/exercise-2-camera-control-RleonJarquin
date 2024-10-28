9ce440153e3afb4896ca950b2d2706d59a239be2
Ruben Leon <98782056+RleonJarquin@users.noreply.github.com>
2024-10-28 15:06:12 -0700

    Created Initial Lock Camera

---
+++ Obscura/scripts/camera_controllers/position_lock.gd
@@ -0,0 +1,63 @@
+class_name positionLock
+extends CameraControllerBase
+
+@export var box_width:float = 5.0
+@export var box_height:float = 5.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	position = target.position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position = target.position
+	
+	if draw_camera_logic:
+		draw_logic()
+	
+func draw_logic() -> void:
+	var mesh_instance = MeshInstance3D.new()
+	var immediate_mesh = ImmediateMesh.new()
+	var material = ORMMaterial3D.new()
+	
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	var left:float = -box_width / 2
+	var right:float = box_width / 2
+	var top:float = -box_height / 2
+	var bottom:float = box_height / 2
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
+	
+	#Add the diagonal vertices
+	#immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+
+	
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.BLACK
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+	
+	#mesh is freed after one update of _process
+	await get_tree().process_frame
+	mesh_instance.queue_free()
+	
+	
